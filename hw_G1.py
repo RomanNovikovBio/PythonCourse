903a884cf1d4564761a3f0c105d28f6b3e91e99d
r-novikov <36457079+RomanNovikovBio@users.noreply.github.com>
2019-04-26 17:17:32 +0300

    Add files via upload

---
+++ hw_G1.py
@@ -0,0 +1,7 @@
+def decorator(func):
+    def wrapped(*args, **kwargs):
+        result = func(*args, **kwargs)
+        f = open(f'{func.__name__}.txt', 'w')
+        f.write(str(result))
+        f.close()
+    return wrapped
